64fa4a04ec703e64348c9a4c3e5ce7d0875e97e9
Andriy-24 <andriymov4ko24@gmail.com>
2024-02-20 15:29:19 +0200

    UserDetails

--- src/components/UserContainer/User/User.js
+++ src/components/UserContainer/User/User.js
@@ -1,9 +1,10 @@
 const User = ({user, getCurrentUser}) => {
-    const {id, name} = user;
+    const {id, name, username} = user;
     return (
         <div>
             <div>id: {id}</div>
             <div>name: {name}</div>
+            <div>username: {username}</div>
             <button onClick={()=>getCurrentUser(user)}>Details</button>
         </div>
     );
